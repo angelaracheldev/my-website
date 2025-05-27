6e7d17769db96dbad04ded11c8ac29bead4e8c6e
Angela Rachel <angelaracheldev@gmail.com>
2025-05-27 21:55:52 +0800

    Test once more jealarr

--- src/app/components/LeftContent.tsx
+++ src/app/components/LeftContent.tsx
@@ -9,7 +9,7 @@ export default function LeftContent() {
   return (
      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[40%] lg:flex-col lg:justify-between lg:py-24">
           <div>
-          <a href="https://angelarachel.dev"><h1 className="text-4xl lg:text-5xl font-bold mb-2"> AngelaRachel </h1></a>
+          <a href="https://angelarachel.dev"><h1 className="text-4xl lg:text-5xl font-bold mb-2"> Jealarr Joseph </h1></a>
           <h2 className="text-2xl lg:text-3xl mb-2"> Full-Stack-ish </h2>
           <Navigations/>
           </div> 
